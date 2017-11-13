8facb921d890a1bb87eafa0520c928ca175a64a7
Aleksi Nuuja <aleksi.nuuja@gmail.com>
2017-11-13 14:36:13 +0200

    built until lesson 3.5

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 
-const store = Redux.createStore(function (state, action) { 
+const addDeck = name => ({ type: 'ADD_DECK', data: name });
+const showAddDeck = () => ({ type: 'SHOW_ADD_DECK'});
+const hideAddDeck = () => ({ type: 'HIDE_ADD_DECK'});
+
+const cards = (state, action) => {
     switch(action.type) {
 
         case 'ADD_CARD':
@@ -8,30 +12,91 @@ const store = Redux.createStore(function (state, action) {
                 id: +new Date
             });
 
-            return Object.assign({}, state, {
-                cards: state.cards ? state.cards.concat([newCard]) : [newCard]
-            });
+            return state.concat([newCard]);
         default:
-            return state || {};
+            return state || [];
+    }
+
+}
+
+const decks = (state, action) => {
+    switch(action.type) {
+        case 'ADD_DECK':
+            let newDeck = { name: action.data, id: +new Date };
+            return state.concat([newDeck]);
+
+        default:
+            return state || [];    
+    }
+}
+
+const addingDeck = (state, action) => {
+    switch(action.type) {
+        case 'SHOW_ADD_DECK': return true;
+        case 'HIDE_ADD_DECK': return false;
+        default: return !!state; // always returns false
+    }
+}
+
+const store = Redux.createStore(Redux.combineReducers({
+    cards,
+    decks,
+    addingDeck
+}));
+
+const App = (props) => {
+    return(
+        <div className='app' >
+            {props.children}
+            </div>
+    );
+};
+
+const Sidebar = React.createClass({
+    componentDidUpdate(){
+        var el = ReactDOM.findDOMNode(this.refs.add);
+        if (el) el.focus();
+    },
+    render() {
+        let props = this.props;
+
+        return (<div className='sidebar'>
+            <h2> All Decks </h2>
+            <button onClick={ e => this.props.showAddDeck() }>
+                New Deck
+            </button>
+            <ul>
+            {props.decks.map((deck, i) =>
+                <li key={i}> {deck.name}</li>
+            )}
+            </ul>
+            { props.addingDeck && <input ref='add' onKeyPress={this.createDeck} /> }
+        </div>);
+    },
+    createDeck(evt) {
+        if (evt.which !== 13) return;
+        var name = ReactDOM.findDOMNode(this.refs.add).value;
+        this.props.addDeck(name);
+        this.props.hideAddDeck();
     }
 });
 
-store.subscribe(() => {
-    console.log(store.getState());
-});
+function run() {
+    let state = store.getState();
 
-store.dispatch({
-    type: 'ADD_CARD',
-    data: {
-        front: 'front',
-        back: 'back'
-    }
-});
+    ReactDOM.render((<App> 
+    <Sidebar 
+    decks={state.decks} 
+    addingDeck={state.addingDeck}
+    
+    addDeck={name => store.dispatch(addDeck(name))}
+    showAddDeck={() => store.dispatch(showAddDeck())}
+    hideAddDeck={() => store.dispatch(hideAddDeck())}
 
-store.dispatch({
-    type: 'ADD_CARD',
-    data: {
-        front: 'front2',
-        back: 'back2'
-    }
-});
+    />
+    </App>), document.getElementById('root'));
+}
+
+run();
+
+store.subscribe(run);
